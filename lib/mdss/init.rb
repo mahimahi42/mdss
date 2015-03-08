14a6292992bc4738d5a6e9ee5b93fda1f51661a5
Bryce Davis <bad1128@gmail.com>
2015-03-08 19:34:59 -0400

    Init now makes dir stubs for CSS, JS, and images

--- lib/mdss/init.rb
+++ lib/mdss/init.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require_relative 'extra'
 
 class MDSSInit
@@ -8,6 +9,36 @@ class MDSSInit
 
         File.open("#{name}/.mdss.cfg", 'w+') do |cfg|
             cfg.write("name=#{name}")
-        end        
+        end
+
+        self.css(name)
+        self.img(name)
+        self.js(name)        
+    end
+
+    def self.css(name)
+        if File.exists?("#{name}/css")
+            MDSSExtra.new.puts_warn("CSS folder exists, skipping")
+        else
+            Dir.mkdir("#{name}/css")
+            FileUtils.touch("#{name}/css/styles.css")
+        end
+    end
+
+    def self.img(name)
+        if File.exists?("#{name}/img")
+            MDSSExtra.new.puts_warn("Image folder exists, skipping")
+        else
+            Dir.mkdir("#{name}/img")
+        end
+    end
+
+    def self.js(name)
+        if File.exists?("#{name}/js")
+            MDSSExtra.new.puts_warn("JavaScript folder exists, skipping")
+        else
+            Dir.mkdir("#{name}/js")
+            FileUtils.touch("#{name}/js/scripts.js")
+        end
     end
 end
--- mdss.gemspec
+++ mdss.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
     s.name        = 'mdss'
     s.version     = '0.0.2'
-#    s.executables << 'mdss'
+    s.executables << 'mdss'
     s.date        = '2015-03-08'
     s.summary     = "Simple static sites with Markdown"
     s.description = "mdss (Markdown Static Sites) allows the creation of static sites via Markdown, specifically GFM"
     s.authors     = ["Bryce Davis"]
     s.email       = 'me@bryceadavis.com'
-    s.files       = ["lib/mdss.rb"]
+    s.files       = Dir["lib/**/*.rb"]
     s.homepage    = 'http://rubygems.org/gems/mdss'
     s.license     = 'MIT'
 end
